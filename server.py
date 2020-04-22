0da736bec57a018f0495796cf7a2e12af9d6b470
anirban-s <baban.chrome@gmail.com>
2020-04-23 05:06:58 +0530

    contact page updated with csv

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
@@ -12,3 +12,26 @@ def my_home():
 def works(page_name):
     return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+    	data = request.form.to_dict()
+    	write_to_csv(data)
+    	return redirect('/thankyou.html')
+    else:
+    	return 'Something went wrong. Please try again.'
+
+def write_to_file(data):
+	with open('database.txt', mode='a') as database:
+		email = data['email']
+		subject = data['subject']
+		message = data['message']
+		file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.csv', mode='a', newline = '') as database:
+		email = data['email']
+		subject = data['subject']
+		message = data['message']
+		csv_writer = csv.writer(database, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
